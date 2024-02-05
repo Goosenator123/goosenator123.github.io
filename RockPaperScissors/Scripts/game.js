0d5575dcdd11ac5fbe2483a023261435017f09df
Simon Cao <simon.cao2007@gmail.com>
2024-02-05 15:59:18 -0500

    Finished the fundation of the game UI

--- RockPaperScissors/Scripts/game.js
+++ RockPaperScissors/Scripts/game.js
@@ -1,8 +1,29 @@
 const playerRock = document.getElementById('player-rock');
+const playerContainer = document.getElementById('player-choice-container');
+const item = document.getElementsByClassName('player-choice');
+
+function moveForward(choice) {
+    let x = document.getElementById('player-choice-container').offsetWidth * 0.35;
+
+    choice.style.transform = `translate(${x}px)`;
+    setTimeout(() => {
+        choice.style.transform = 'none';
+    }, 1000);
+}
+
+// for (let i in item) {
+//     i.addEventListener('click', () => {
+//         moveForward(item);
+//         console.log('olasfds')
+//     })
+// }
+
+item.forEach(element => {
+    element.addEventListener('click', () => {
+        moveForward(element);
+    })
+});
 
 playerRock.addEventListener('click', () => {
-    playerRock.style.transform = 'translate(200px)';
-    setTimeout(()=>{
-        playerRock.style.transform = 'none';
-    }, 1000)
+    moveForward(playerRock);
 })
