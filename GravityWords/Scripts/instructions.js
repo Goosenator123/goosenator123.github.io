d86cfb8d84d958625dd848615dd190262dfee89f
Simon Cao <simon.cao2007@gmail.com>
2024-02-26 22:12:48 -0500

    Implemented button for people that dont have keyboard for show/hide settings

--- GravityWords/Scripts/instructions.js
+++ GravityWords/Scripts/instructions.js
@@ -1,8 +1,18 @@
 const infoSection = document.getElementById('info-section');
+const settingButton = document.getElementById('setting-button');
 let zPosition = 100;
+
+function putBack () {
+    zPosition = -zPosition
+    infoSection.style.zIndex = zPosition; // Set z-index to a value that places the element behind others
+}
+
 window.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') { // Check if the key code is equal to the keycode of the "Escape" key
-        zPosition = -zPosition
-        infoSection.style.zIndex = zPosition; // Set z-index to a value that places the element behind others
+        putBack();
     }
 });
+
+settingButton.addEventListener('click', () => {
+    putBack();
+})
