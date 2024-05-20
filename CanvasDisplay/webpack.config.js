1d54cf5fd28821dc53ba14a0d54904c68d7f3853
Simon Cao <simon.cao2007@gmail.com>
2024-05-20 00:10:19 -0400

    Initial commit

---
+++ CanvasDisplay/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/scripts/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name][contenthash].js',
+        clean: true,
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+        watchFiles: ['src/**/*'], // Watch all files in the src directory
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ],
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            chunks: ['bundle'],
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+    ],
+}
